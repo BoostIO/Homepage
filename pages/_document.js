cd05585b84817ba108d5bf240d110d16592bdc12
Junyoung Choi <fluke8259@gmail.com>
2018-02-04 20:08:59 +0900

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,14 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <meta name='viewport' content='width=device-width' />
+
+          <link rel='apple-touch-icon' sizes='180x180' href='/static/favicon/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon/favicon-16x16.png' />
+          <link rel='manifest' href='/static/favicon/site.webmanifest' />
+          <meta name='msapplication-TileColor' content='#00aba9' />
+          <meta name='theme-color' content='#ffffff' />
+
           {this.props.styleTags}
         </Head>
         <body>
